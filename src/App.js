2515bb823ee51abf673dba8d3b5a1e205db38bd6
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-04-04 23:36:41 -0400

    implemented form component with post request

--- src/App.js
+++ src/App.js
@@ -1,49 +1,36 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import Tweet from './components/Tweet';
+import Form from './components/Form';
 const axios = require('axios').default;
 
 function App() {
 
-  const [tweetsData, setTweetsData] = useState([]);
+    const [tweetsData, setTweetsData] = useState([]);
 
-  const retrieveTweets = async () => {
-    const result = await axios({
-      method: 'get',
-      url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
-      withCredentials: true,
-    });
-    //console.log(result);
-    //console.log(result.data);
-    setTweetsData(result.data);
-    return result;
-  }
+    // Makes a get request to the tweets database
+    const retrieveTweets = async () => {
+        const result = await axios({
+            method: 'get',
+            url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
+            withCredentials: true,
+        });
+        setTweetsData(result.data);
+        return result;
+    }
 
-  const postTweet = async () => {
-    const result = await axios({
-      method: 'post',
-      url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
-      withCredentials: true,
-      data: {
-        body: "testing"
-      },
-    });
-    return result;
-    // we are going to have a text field
-    // this function will have a parameter that is the text
-    // the body will have value of the parameter
-  }
-
-  useEffect(() => {
-    retrieveTweets();
-    //postTweet();
-  }, []);
+    useEffect(() => {
+        retrieveTweets();
+    }, []);
 
   return (
     <div className="App">
-      {tweetsData.slice([0], [50]).map(x => {
-        return <Tweet tweetID = {x.id} key = {x.id}/>
-      })}
+        <Form />
+        <div>
+            {tweetsData.slice([0], [50]).map(x => {
+                return <Tweet tweetID = {x.id} key = {x.id}/>
+            })}
+        </div>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+const axios = require('axios').default;
+// import css if you need to
+
+// ideas
+// have this form component on top of website
+// make a tweet button that displays a white div overlay screen that also generates the form component
+
+function Name() {
+
+    const [text, setText] = useState("");   // represents the text body
+
+    // Adds tweet to the database
+    const postTweet = async () => {
+        // Will not post if text field is empty
+        if (text === "") {
+            return;
+        }
+        const result = await axios({
+            method: 'post',
+            url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
+            withCredentials: true,
+            data: {
+                body: text
+            },
+        });
+        console.log(text);
+        return result;
+    }
+
+    const deleteTweet = async () => {
+        const result = await axios({
+            method: 'delete',
+            url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets/39691',
+            withCredentials: true,
+        });
+        return result;
+    }
+
+    // const oof = () => {
+    //     for (let i = 39655; i<= 39682; i++) {
+    //         deleteTweet(i);
+    //     }
+    // }
+
+    useEffect(() => {
+        //deleteTweet();
+    }, []);
+    
+    //onSubmit={postTweet()}
+
+    return (
+        <div className="App">
+            <div>
+                <form>
+                    <textarea onChange={e => setText(e.target.value)} value={text}> </textarea>
+                    <button onClick={e => {e.preventDefault(); postTweet(); setText("")}}>Tweet</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Name;
